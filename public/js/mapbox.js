c8bfaef30e430c3faf3aaef299d5c29795c8c4fe
Philip <philipelias16@gmail.com>
2023-11-10 02:03:45 +0100

    first commit

---
+++ public/js/mapbox.js
@@ -0,0 +1,49 @@
+export const displayMap = locations => {
+    mapboxgl.accessToken =
+      'pk.eyJ1IjoidGhlYmFieWdvYXQiLCJhIjoiY2xvcmhqOXJzMHVsNDJqbzYwb2RmbmRxcCJ9.XP39OtmoFqSaBNF5VRbyFw';
+  
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'https://studio.mapbox.com/styles/thebabygoat/clorujizg00ns01nzcvkahqfq/edit/#11/40.73/-74',
+      scrollZoom: false
+      // center: [-118.113491, 34.111745],
+      // zoom: 10,
+      // interactive: false
+    });
+  
+    const bounds = new mapboxgl.LngLatBounds();
+  
+    locations.forEach(loc => {
+      // Create marker
+      const el = document.createElement('div');
+      el.className = 'marker';
+  
+      // Add marker
+      new mapboxgl.Marker({
+        element: el,
+        anchor: 'bottom'
+      })
+        .setLngLat(loc.coordinates)
+        .addTo(map);
+  
+      // Add popup
+      new mapboxgl.Popup({
+        offset: 30
+      })
+        .setLngLat(loc.coordinates)
+        .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+        .addTo(map);
+  
+      // Extend map bounds to include current location
+      bounds.extend(loc.coordinates);
+    });
+  
+    map.fitBounds(bounds, {
+      padding: {
+        top: 200,
+        bottom: 150,
+        left: 100,
+        right: 100
+      }
+    });
+  };
---
+++ server.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
+
+// mongoose
+//   .connect(DB,{
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+//   })
+//   .then(() => console.log('DB connection successful!'));
+
+mongoose
+  .connect('mongodb+srv://philipelias16:Kewoyoung1620@cluster0.bejvujp.mongodb.net/?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successful!'));
+
+// mongoose
+//   .connect('mongodb+srv://philipelias16:Kewoyoung1620@cluster0.bejvujp.mongodb.net/TourNow?retryWrites=true&w=majority');
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
